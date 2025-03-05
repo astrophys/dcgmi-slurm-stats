362d7768eee74a8d19f1056e4f4b3df043f62fdc
Ali Snedden <ali.snedden@markiiisys.com>
2025-03-05 13:30:56 -0500

    submit_2task_2node.sh : 1. Got it running on two nodes
    submit_2task_1node.sh :
    1. Got it running on 1 node

--- submit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH --gres=gpu:3
-#SBATCH --cpus-per-task=16
-
-ml load nvhpc/24.7
-echo "hello world"
---
+++ submit_2task_1node.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --ntasks=2
+#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-task=16
+
+module use /home/${USER}/software/hpc_sdk_2023_239/modulefiles
+ml load nvhpc-hpcx-cuda12/23.9
+mpiexec --np 2 ./mpi_matrix_mult --option mpi_gpu  --size 10 --verbose true
+

01e017301e3dd5f32281773db97d84d86ab91274
Ali Snedden <ali.snedden@markiiisys.com>
2025-03-06 13:38:44 -0500

    submit_2task_1node.sh : 1. Got rough outline of setting up dcgmi monitoring for my job.    --> KEY CHALLENGE : CUDA_VISIBLE_DEVICES' indexing DOES NOT follow dcgmi's                        This means if you're allocated gpu's 0, 2 on a node (per dcgmi's                        indexing), CUDA_VISIBLE_DEVICES=0,1 b/c it ALWAYS starts at 0.

--- submit_2task_1node.sh
+++ submit_2task_1node.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH --ntasks=2
-#SBATCH --gres=gpu:2
-#SBATCH --cpus-per-task=16
+#SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-task=1
 #SBATCH --output=slurm-out/slurm-%A.out
 
+###SBATCH --gres=gpu:1
 echo "TMPDIR = ${TMPDIR}"
 export TMPDIR=${SLURM_SUBMIT_DIR}
 echo "TMPDIR = ${TMPDIR}"
@@ -15,25 +16,27 @@ if [ -n ${CUDA_VISIBLE_DEVICES} ]; then
     export TMPDIR=${TMPDIR}/${SLURM_JOB_ID}
     
     # Sanity check that CUDA_VISIBLE_DEVICES are same as seen by nvidia-smi
-    export smivis=
-    for i in `nvidia-smi -L | awk '{print $2}' | cut -d":" -f1 `; do
-        if [ -z $smivis ]; then
-            smivis=${i};
-        else
-            echo $smivis;
-            smivis="${smivis},${i}";
-        fi;
-    done 
-    if [ $smivis == $CUDA_VISIBLE_DEVICES ]; then
-        echo "true  : nvidia-smi -L == CUDA_VISIBLE_DEVICES : $CUDA_VISIBLE_DEVICES";
-    else
-        echo "false : nvidia-smi -L == CUDA_VISIBLE_DEVICES : $CUDA_VISIBLE_DEVICES";
-    fi
+    #export smivis=
+    #for i in `nvidia-smi -L | awk '{print $2}' | cut -d":" -f1 `; do
+    #    if [ -z $smivis ]; then
+    #        smivis=${i};
+    #    else
+    #        echo $smivis;
+    #        smivis="${smivis},${i}";
+    #    fi;
+    #done 
+    #if [ $smivis == $CUDA_VISIBLE_DEVICES ]; then
+    #    echo "true  : nvidia-smi -L == CUDA_VISIBLE_DEVICES : $CUDA_VISIBLE_DEVICES";
+    #else
+    #    echo "false : nvidia-smi -L == CUDA_VISIBLE_DEVICES : $CUDA_VISIBLE_DEVICES";
+    #fi
 
     ## You must create a 'group' with the GPUs of interest
     ## THEN you must start monitoring with a 'dcgmi job'
     ## QUESTION : How do I list the running dcgmi jobs?
-    stdout=$(dcgmi group -c job_${SLURM_JOB_ID} -a ${CUDA_VISIBLE_DEVICES})
+    # stdout=$(dcgmi group -c job_${SLURM_JOB_ID} -a ${CUDA_VISIBLE_DEVICES})
+
+    stdout=$(dcgmi group -c job_${SLURM_JOB_ID} -a ${SLURM_STEP_GPUS})
     if [ $? -eq 0 ]; then
         groupid=$(echo $stdout | awk '{print $10}')
         dcgmi stats -e
@@ -43,10 +46,17 @@ if [ -n ${CUDA_VISIBLE_DEVICES} ]; then
     fi
 fi
 
-
+nvidia-smi -L
+set | grep "CUDA\|SLURM" &> ${TMPDIR}/set_slurm_env
 module use /home/${USER}/software/hpc_sdk_2023_239/modulefiles
 ml load nvhpc-hpcx-cuda12/23.9
-mpiexec --np 2 ./mpi_matrix_mult --option mpi_gpu  --size 100 --verbose false
+### NOTE : mpiexec doesn't work right here with GPUs, if used all your processes will run
+### on the same GPU
+#mpiexec --np 2 ./mpi_matrix_mult --option mpi_gpu  --size 500 --verbose false & 
+srun ./mpi_matrix_mult --option mpi_gpu  --size 500 --verbose false &
+sleep 10
+nvidia-smi  &> ${TMPDIR}/nvidia-smi_running
+wait
 
 
 
